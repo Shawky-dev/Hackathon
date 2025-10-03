6493178444bea864137d8e8aa3e78c042b2712d6
shawky-dev <shawky.projects25@gmail.com>
2025-10-03 15:36:09 +0300

    -project structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,19 @@
+//___Import
 const express = require('express')
+const cors = require("cors")
+const dotenv = require("dotenv").config()
+//___Intialize
 const app = express()
 const port = 8000
 
+
+//___Middlewares
+app.use(cors()) //ALL CORS REQUESTS ARE ENABLED⚠️⚠️
+app.use(express.json())
+app.use(cookieParser())
+
+
+//___Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
