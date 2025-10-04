a7d36592cf9e46db55c37c45ab51ef908dda4f33
Zeyad Y. Shaban <64459068+zeyad-shaban@users.noreply.github.com>
2025-10-05 01:55:55 +0200

    api data integrated correclty for teh ai code

--- ai_api/main.py
+++ ai_api/main.py
@@ -7,18 +7,6 @@ from darts.models import NBEATSModel
 from typing import Dict, List
 from pathlib import Path
 
-from utils.preprocessing_utils import preprocess_input_data
-from ai_api.utils.forecast_utils import get_raw_prediction
-
-BASE = Path(__file__).resolve().parent
-
-# PATHS
-GASSES_MODEL_PATH = BASE / "static" / "co_no2_co3_no2_nbeats.pth"
-PARTICULATES_MODEL_PATH = BASE / "static" / "pm25frm_pm10mass_pmc_mass_nbeats.pth"
-
-# Load models once at startup
-model_gasses = NBEATSModel.load(str(GASSES_MODEL_PATH))
-model_particulates = NBEATSModel.load(str(PARTICULATES_MODEL_PATH))
 
 def ai_api():
     pass
